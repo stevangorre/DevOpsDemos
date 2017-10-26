9cdce81a4215c14cd0de75f165f29eb4e2595e24
gorre <stevan.gorre@capgemini.com>
2017-10-26 16:07:53 +0530

    Test Java version 1.8 added

--- Test.java
+++ Test.java
@@ -3,7 +3,6 @@
 public class Test{
 
          public static void main(String[] args) {
-			System.out.println("This is  master");
 			System.out.println("This is by Tester");
 			System.out.println("This is by master after merge");
 			System.out.println("cool");
