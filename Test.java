af996d81c36cae0c084adaeb16b95185d3a93b2a
gorre <stevan.gorre@capgemini.com>
2017-10-26 15:55:24 +0530

    Test Java version 1.5 added

--- Test.java
+++ Test.java
@@ -5,6 +5,7 @@ public class Test{
          public static void main(String[] args) {
 			System.out.println("This is  master");
 			System.out.println("This is by Tester");
+			System.out.println("This is by master after merge");
 	}
 
 }
