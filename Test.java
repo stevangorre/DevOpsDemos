adb37c1490226f557def74a085b6e951f17e8971
gorre <stevan.gorre@capgemini.com>
2017-10-26 16:06:22 +0530

    Test Java version 1.7 added

--- Test.java
+++ Test.java
@@ -7,6 +7,7 @@ public class Test{
 			System.out.println("This is by Tester");
 			System.out.println("This is by master after merge");
 			System.out.println("cool");
+			System.out.println("Great");
 	}
 
 }
